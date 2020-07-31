7b9eeb68d9e143833bede49dfa4641466ab1e3a7
RyanGeorgeZA <62384839+RyanGeorgeZA@users.noreply.github.com>
2020-07-31 17:11:08 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'To do list';
+  todos = [ 
+    {
+      label: "Bring Milk",
+      done: false,
+      priority: 3
+    },
+    {
+      label: "Pay mobile bill",
+      done: true,
+      priority: 1
+    },
+    {
+      label: "Clean house",
+      done: false,
+      priority: 4
+    },
+    {
+      label: "Fix the bulb",
+      done: false,
+      priority: 5
+    },
+];
+
+addTodo(newTodoLabel) {
+  var newTodo = {
+    label: newTodoLabel,
+    priority: 1,
+    done: false
+  };
+  this.todos.push(newTodo);
+}
+
+deleteTodo(todo) {
+  this.todos = this.todos.filter( t => t.label !== todo.label );
+}
+}
